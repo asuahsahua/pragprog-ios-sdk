fecb50a3a80a2cb1dbe08bc67e6560cc8b478150
Natalie Marion <asuahsahua@gmail.com>
2017-09-23 22:22:18 -0400

    End of chapter 7

--- PragPods/ViewController.swift
+++ PragPods/ViewController.swift
@@ -11,11 +11,15 @@ import AVFoundation
 import CoreMedia
 
 class ViewController: UIViewController {
-    public var player: AVPlayer?
+    var player: AVPlayer?
     
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var playPauseButton: UIButton!
     @IBOutlet var logoView: UIImageView!
+    @IBOutlet var timeLabel: UILabel!
+
+    // Observer for the player's timer
+    private var playerPeriodicObserver: Any?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,10 +48,21 @@ class ViewController: UIViewController {
         player = AVPlayer(url: url);
         titleLabel.text = url.lastPathComponent
         player?.addObserver(self, forKeyPath: "rate", options: [], context: nil)
+
+        let interval = CMTime(seconds: 0.25, preferredTimescale: 1000)
+        playerPeriodicObserver = player?.addPeriodicTimeObserver(forInterval: interval, queue: nil) {
+            [weak self] currentTime in self?.updateTimeLabel(currentTime)
+        }
     }
 
-    deinit {
-        player?.removeObserver(self, forKeyPath: "rate")
+    func updateTimeLabel(_ time: CMTime) {
+        let formatter = DateComponentsFormatter()
+
+        formatter.unitsStyle = .positional
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.zeroFormattingBehavior = .pad
+
+        timeLabel.text = formatter.string(from: TimeInterval(time.seconds))
     }
 
     override func observeValue(forKeyPath keyPath: String?,
@@ -57,6 +72,14 @@ class ViewController: UIViewController {
     {
         if keyPath == "rate", let player = object as? AVPlayer {
             playPauseButton.setTitle(player.rate == 0 ? "Play" : "Pause", for: .normal)
+        }
+    }
+
+    deinit {
+        player?.removeObserver(self, forKeyPath: "rate")
+
+        if let observer = playerPeriodicObserver {
+            player?.removeTimeObserver(observer)
         }
     }
 }
